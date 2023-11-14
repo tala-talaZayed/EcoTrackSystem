b6fef9a28848608929a691d0790a9fa3c63add90
talaZayed2002 <137496459+talaZayed2002@users.noreply.github.com>
2023-11-14 20:53:34 +0200

    Delete app.js

--- app.js
+++
@@ -1,9 +0,0 @@
-const express = require ("express");//like import 
-const app = express();
-const userRouter = require("./API_Users/users/user.router");
-app.use(express.json());
-
-app.use("/API_Users/users",userRouter);
-app.listen(3000, ()=>{
-    console.log("hi");
-});
