7152d70afc9baaa42b29bfacc71394920ab01523
Tala Alzayed <s12028357@stu.najah.edu>
2023-11-20 16:15:50 +0200

    commit

--- EcoTrackSystem/API/users/user.router.js
+++ EcoTrackSystem/API/users/user.router.js
@@ -4,14 +4,15 @@ const {createUser,getUsers,getUserByEmail,deleteUser,
     getUsersBySimilarLocation,
     getUsersByUserName} = require("./user.controller");      
 const {checkToken}= require("../../Authoriaztion/tokenValidation");
-
 router.post("/",createUser);
 router.post("/login",login);
 router.get("/",checkToken,/*checkToken,*/getUsers);
 router.get("/Email/:Email",getUserByEmail);
 router.get("/:UserName",getUsersByUserName);
-router.delete("/:UserId",/*checkToken,*/deleteUser);
-router.patch("/" ,/*checkToken,*/updateUser );
+//for current user 
+router.delete("/",/*checkToken,*/deleteUser);
+router.patch("/",/*checkToken,*/updateUser );
+//for current user ^
 router.get("/Interests/:MostIntersets",/*checkToken,*/getUsersBySimilarInterests);
 router.get("/Location/:Location",/*checkToken,*/getUsersBySimilarLocation);
 module.exports = router ;
